539e77d85f3a432b656671f7c3a9b8823e48e000
Deepshikha Das <ded218@lehigh.edu>
2014-08-30 22:43:16 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,30 @@
+///////////////////////////////////////////////////////////
+//Deepshikha Das
+//Welcome Class Java Program
+//
+//  i am compiling the program
+//      javac WelcomeClass.java
+//  running the program
+//      java WelcomeClass//
+
+// defining the class
+public class WelcomeClass{
+    
+//  adding main method
+    public static void main(String[] args) {
+        
+//  printing the statement with a box around it
+    System.out.println("          -----------           ");
+    System.out.println("          |WELCOME |            ");
+    System.out.println("          -----------           ");
+    System.out.println("      ^   ^   ^   ^   ^   ^     ");
+    System.out.println("     / \\/ \\/ \\/ \\/ \\/ \\   ");
+    System.out.println("     |-D--E--D--2--1--8-|       ");
+    System.out.println("    \\ /\\ /\\ /\\ /\\ /\\ /    ");
+    System.out.println("      v   v   v   v   v         ");
+    System.out.println("Tiramisu is my favorite dessert.");
+    System.out.println("test");
+        
+    //test/testtest
+    }
+}
