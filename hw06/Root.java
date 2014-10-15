d51e6e46cdd326d63a5d0f12ba33482eeeed9a32
Deepshikha Das <ded218@lehigh.edu>
2014-10-15 02:42:06 +0000

    done

--- hw06/Root.java
+++ hw06/Root.java
@@ -14,23 +14,29 @@ public class Root {
     Scanner myScanner;  //declaring instance of scanner object
     myScanner = new Scanner( System.in ); //calling the scanner constructor
     System.out.println("Enter a number"); //prompting the user
-        double x = myScanner.nextDouble(); //accepting user input
-        double low = 0;//setting low 
-        double high = 1 + x; //setting the high 
-    while(true){ //infite loop
-        double middle = (low+high)/2; //calculating the middle of the low and the high 
-        double middleSqr = middle* middle; //calculating the square of the middle
-        if(middleSqr> x){ //if statement for if the sqrdmiddle number is greater than x
-            high=middle; //making the high equal the middle 
-        } // end of if1
-        if(middleSqr<x){ //if statement for if the sqrdmiddle number is less than x
-            low=middle; //making the low equal the middle 
-        }//end of if2
-        double interval = high-low; //declaring the interval
-        if(interval <=0.0000001*high){ //if statment to end loop
-            System.out.println("The square root is "+middle+"."); //printing the square root
-            break; //ending loop
-        }//end of if statement for break
-    }//end of infinite while loop
+    double x = myScanner.nextDouble(); //accepting user input
+    if (x>0){
+            double low = 0;//setting low 
+            double high = 1 + x; //setting the high 
+        while(true){ //infite loop
+            double middle = (low+high)/2; //calculating the middle of the low and the high 
+            double middleSqr = middle* middle; //calculating the square of the middle
+            if(middleSqr> x){ //if statement for if the sqrdmiddle number is greater than x
+                high=middle; //making the high equal the middle 
+            } // end of if1
+            if(middleSqr<x){ //if statement for if the sqrdmiddle number is less than x
+                low=middle; //making the low equal the middle 
+            }//end of if2
+            double interval = high-low; //declaring the interval
+            if(interval <=0.0000001*high){ //if statment to end loop
+                System.out.println("The square root is "+middle+"."); //printing the square root
+                break; //ending loop
+            }//end of if statement for break
+        }//end of infinite while loop
+    }//end of overall if statement
+    else{
+        System.out.println("You did not enter an double greater than zero."); //output to tell user they did not enter the right input
+        return; //end program 
+    }//end of else statement 
     }//end of main method
 }//end of class
