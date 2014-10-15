a8c498766ecda571480736fdfbe2d5120c5df0b7
Deepshikha Das <ded218@lehigh.edu>
2014-10-15 02:00:45 +0000

    done

--- hw06/Root.java
+++ hw06/Root.java
@@ -17,20 +17,19 @@ public class Root {
         double x = myScanner.nextDouble(); //accepting user input
         double low = 0;//setting low 
         double high = 1 + x; //setting the high 
-    while(true){
-        double middle = (low+high)/2; 
-        double middleSqr = middle* middle; 
-        if(middleSqr> x){
-            high=middle; 
+    while(true){ //infite loop
+        double middle = (low+high)/2; //calculating the middle of the low and the high 
+        double middleSqr = middle* middle; //calculating the square of the middle
+        if(middleSqr> x){ //if statement for if the sqrdmiddle number is greater than x
+            high=middle; //making the high equal the middle 
         } // end of if1
-        if(middleSqr<x){
-            low=middle;
+        if(middleSqr<x){ //if statement for if the sqrdmiddle number is less than x
+            low=middle; //making the low equal the middle 
         }//end of if2
-        double interval = high-low; 
-        if(interval <=0.0000001*high){
-            double cInterval = interval/2;
-            System.out.println("The square root is "+middle+".");
-            break; 
+        double interval = high-low; //declaring the interval
+        if(interval <=0.0000001*high){ //if statment to end loop
+            System.out.println("The square root is "+middle+"."); //printing the square root
+            break; //ending loop
         }//end of if statement for break
     }//end of infinite while loop
     }//end of main method
