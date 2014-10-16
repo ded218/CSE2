7c48dd6d95e2d2cc0137db1b3d382f8f0db69375
Deepshikha Das <ded218@lehigh.edu>
2014-10-16 03:10:29 +0000

    done

--- lab04/BigMacAgain.java
+++ lab04/BigMacAgain.java
@@ -10,23 +10,36 @@ import java.util.Scanner; //i am importing the scanner
 //
 //
 public class BigMacAgain {
-        public static void main(String[] args) {
-        Scanner myScanner;
-        myScanner = new Scanner( System.in );
-        System.out.print("Enter the number of Big Macs: "); // prompting the user for the number of big macs
-            int nBigMacs = myScanner.nextInt();
-        if(myScanner.hasNextInt())
-            if(nBigMacs<0){
-            double BigPrice$ = nBigMacs*2.22;
-            System.out.println("You ordered "+nBigMacs+"for a cost of"+BigPrice$+".");
-        }
+    //adding main method which is always necessary 
+    public static void main(String[] args) {
+    Scanner myScanner; //declaring instance of a scanner object
+    myScanner = new Scanner( System.in ); // calling the scanner constructor
+    System.out.print("Enter the number of Big Macs: "); // prompting the user for the number of big macs
+    if(myScanner.hasNextInt()){
+        int nBigMacs = myScanner.nextInt(); //accpeting user input for the number of burgers
+        double tCost = nBigMacs*2.22;
+        if(nBigMacs > 0){
+            System.out.println("You ordered "+nBigMacs+" for a cost of "+nBigMacs+"x2.22 = $"+tCost+".");
+        }//end of if statement
         else{
-            System.out.println("You did not enter an int");
-            return; // leaves the program, i.e.
-                        //the p{rogram terminates
-                        
+            System.out.println("You did not enter an int>0.");
+        }//end of else statement 
+        System.out.println("Do you want an order of fries(Y/y/N/n)?");
+        String fries = myScanner.next(); //accepting user input for fries
+        if (fries.equalsIgnoreCase("Y")){
+            double tTCost = tCost + 2.15;
+            System.out.println ("You ordered fries at a cost of $2.15.");
+            System.out.println ("The total cost of the meal is $"+tTCost+".");
         }
-        
-            
+        if (fries.equalsIgnoreCase("N")){
+            System.out.println ("The total cost of the meal is $"+tCost+".");
+            } //end of fries yes if statement
+        else{
+            System.out.println ("You did not enter Y, y, N, or n.");
+        }//end of else statement 
+        }//end of big if statement
+        else{
+            System.out.println("You did not enter an int.");
+        }//end of else statement
         } //end of main method
 } //end of class
